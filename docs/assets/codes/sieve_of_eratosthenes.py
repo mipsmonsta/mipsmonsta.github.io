6095999470bdb0a59061216d14a7a8f590c176f1
mipsmonsta <yaojintham@gmail.com>
2024-03-04 14:00:08 +0800

    variant code

--- docs/assets/codes/sieve_of_eratosthenes.py
+++ docs/assets/codes/sieve_of_eratosthenes.py
@@ -26,3 +26,29 @@ n = 1000
 print(s.primeNumbersUpTo(n))
 
 print(timeit.timeit(f's.primeNumbersUpTo({n})', number=10000, globals=globals()))
+
+
+class Solution():
+    def primeNumbersUpTo(self, n): # inclusive of n
+        primeCandidates = set([i for i in range(2, n+1)])
+
+        for i in range(2, n+1):
+
+            # i is not prime
+            if i not in primeCandidates:
+                continue
+
+            multiples = {multiple for multiple in range(i*i, n+1, i)}
+            primeCandidates -= multiples
+
+        return list(primeCandidates)
+    
+s = Solution()
+n = 100
+print(s.primeNumbersUpTo(n))
+
+s = Solution()
+n = 1000
+print(s.primeNumbersUpTo(n))
+
+print(timeit.timeit(f's.primeNumbersUpTo({n})', number=10000, globals=globals()))
